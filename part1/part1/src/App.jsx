efeee1082662e3e08488754a948ab7f93f2f875c
Lio234 <lionardoayllon@gmail.com>
2024-02-02 17:44:07 -0500

    add part 1

---
+++ part1/part1/src/App.jsx
@@ -0,0 +1,19 @@
+const Mensaje = () => {
+  let a= 12
+  let b=1
+  return <p>{a+b}</p>  
+}
+
+
+function App() {
+  let fecha= new Date ( )
+  return (
+      <div>
+        <Mensaje/>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At iusto quae laborum reprehenderit iste est consequuntur. Perspiciatis perferendis provident, reiciendis, minus tempore ex architecto, hic culpa dolores ut fugit! Optio!</p>
+        <h1>hodfabof</h1> 
+      </div>
+  )
+}
+
+export default App
